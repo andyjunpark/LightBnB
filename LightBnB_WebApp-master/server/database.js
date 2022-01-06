1e57c9f22c76ca84e51b7ebbdedcb236befe28e7
Andy Junhyuk Park <pos03142ap@gmail.com>
2022-01-06 22:32:18 +0000

    implement getUserWithEmail && getUserWithId && addUser

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -18,16 +18,16 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
+  return pool.query(`
+    SELECT * FROM users WHERE email = $1;`, [email])
+    .then(res => {
+    if (res) {
+      return res.rows[0];
     } else {
-      user = null;
+      return null;
     }
-  }
-  return Promise.resolve(user);
+  })
+  .catch(err => console.error('query error', err.message));
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -37,7 +37,18 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool.query(`
+    SELECT * FROM users
+    WHERE id = $1;
+  `, [id])
+  .then(res => {
+    if (res) {
+      return res.rows[0];
+    } else {
+      return null;
+    }
+  })
+  .catch(err => console.error('query error', err.message));
 }
 exports.getUserWithId = getUserWithId;
 
@@ -48,10 +59,14 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+  .query(`
+    INSERT INTO users(name, email, password)
+    VALUES($1 ,$2, $3)
+    RETURNING *;
+  `, [user.name, user.email, user.password])
+  .then(res => res.rows)
+  .catch(err => console.error('query error', err.message));
 }
 exports.addUser = addUser;
 
@@ -85,15 +100,6 @@ exports.getAllReservations = getAllReservations;
     });
 };
   exports.getAllProperties = getAllProperties;
-// const getAllProperties = function(options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// }
-// exports.getAllProperties = getAllProperties;
-
 
 /**
  * Add a property to the database
